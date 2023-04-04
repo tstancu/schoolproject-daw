f6bbf732aae83f4c93d881511daf8b440998ce5a
Theodor Stancu <theodor.stancu@s.unibuc.ro>
2023-04-04 13:01:14 +0300

    added bootstrap and also made ssh connection from local to vm inside vs code

--- public/index.php
+++ public/index.php
@@ -1,9 +1,36 @@
 <html>
-<head>
 
+<head>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
 </head>
+
 <body>
-<p>Anul II - DAW - Revista Online</p>
-<p>Stancu Theodor, ID, grupa 2</p>
+    <p>Anul II - DAW - Revista Online</p>
+    <p>Stancu Theodor, ID, grupa 2</p>
+    <p>BIG TEST 2</p>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">My Online Magazine</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Articles</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </body>
+
 </html>
---
+++ public/test.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Hello, world!"
+
+$missing_semicolon = "This line should have a semicolon at the end"
+
+?>
